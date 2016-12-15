2ea10e05e155bcbc18e193b61585684b85b9b32e
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2016-12-15 13:33:10 -0700

    Polishing my CSS

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
 
+  // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+
+  //getFlights();
+
   //  $(document).ready(function(){
   //   $('ul.tabs').tabs('select_tab', 'tab_id');
   // });
   //
   // //$("#planes").click(function () {
   //   //event.preventDefault()
-  getFlights();
   //addEvent()
   //)}
 
@@ -25,8 +31,7 @@ var latlong = [];
 var map;
 var markers;
 var image;
-var things;
-var content = []
+
 // function getAirline(names) {
 //   $.getJSON("https://iatacodes.org/api/v6/airlines?api_key=772513cb-42b7-4262-b735-00d2f52eb796&code=" + names, function(airline) {
 //     $(".airline").html("")
@@ -71,7 +76,6 @@ var content = []
 
 function getFlights() {
   $.getJSON("https://iatacodes.org/api/v6/flights?api_key=772513cb-42b7-4262-b735-00d2f52eb796", function(flights) {
-    localStorage.setItem("bulk-data", flights)
     displayFlights(flights)
   })
 }
@@ -81,44 +85,40 @@ function displayFlights(data) {
   for (var i = 0; i < data.response.length; i++) {
     var fl = data.response[i];
 
-    //console.log(fl)
-
     var spd = fl.speed.horizontal
     var speed = "Speed: " + spd + " mph"
-    //console.log(spd)
 
     var alt = fl.geography.alt
     var altitude = "Altitude: " + alt + " ft"
-    //console.log(altitude)
 
     var dep = fl.departure_code
     var depart = "Departure: " + dep
-    //console.log(depart)
 
     var arr = fl.arrival_code
     var arrive = "Arrival: " + arr
-    //console.log(arrive)
 
     var flt = fl.flight.name
     var flight = "Flight " + flt
-    //console.log(flight)
 
     var call = fl.flight.callsign
-    var callsign = "Callsign " + call
-    //console.log(callsign)
+    var callsign = "Callsign: " + call
 
     var acc = fl.flight.aircraft_code
-    var acode = "Aircraft type: " + acc
-    //console.log(acode)
+    var acode = "Aircraft: " + acc
 
-    var contentString = speed + " " + "<br/>" + altitude + "<br/>" + depart + "<br/>" + arrive + "<br/>" + flight + "<br/>" + callsign + "<br/>" + acode
+    var contentString = flight.bold() + " " + "<br/>" + depart + "<br/>" + arrive + "<br/>" + altitude + "<br/>" + speed + "<br/>" + acode + "<br/>" + callsign
     contentStrings[i] = contentString
 
     var x = {"lat": fl.geography.lat, "lng": fl.geography.lng}
     latlong[i] = x;
   }
 
-  image = "http://wfarm1.dataknet.com/static/resources/icons/set53/adcf5980.png";
+  var image = {
+    url: "http://www.clker.com/cliparts/T/i/o/c/X/Q/airplane-hi.png", // url
+    scaledSize: new google.maps.Size(28, 28), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor
+};
 
   markers = latlong.map(function(location, i) {
     var thing =  new google.maps.Marker({
@@ -126,8 +126,6 @@ function displayFlights(data) {
       map: map,
       icon: image
       });
-
-      //console.log(contentStrings)
 
       var infowindow = new google.maps.InfoWindow({
         content: contentStrings[i]
@@ -137,34 +135,20 @@ function displayFlights(data) {
         map.setZoom(6);
         map.setCenter(thing.getPosition());
         infowindow.open(map, thing);
-        //infowindow.close()
       });
 
+      google.maps.event.addListener(infowindow, "closeclick", function () {
+        map.panTo(this.getPosition())
+        map.setZoom(3)
+      })
 
       return thing;
-
     });
-
 } // end displayFlights()
-
-// function addEvent(data) {
-//   for (var i = 0; i < latlong.length; i++) {
-//     var j = latlong[i]
-//     console.log(j)
-//     // j.click(function () {
-//     //   console.log("hey")
-//     // })
-//   }
-// }
-//console.log(latlong)
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 3,
-    center: {lat: 39.742043, lng: -81.4183}
+    center: {lat: 39.742043, lng: -104.991531}
   });
-  // map.addListener("click", function (jet) {
-  //   console.log(jet)
-  // })
-
 } // end initMap()
